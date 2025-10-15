07cebef58c2da16db691610d84f25774651a0390
Spellingz <kshspell@gmail.com>
2025-10-15 13:42:47 +0200

    added gitignore

---
+++ 4fun/idk.c
@@ -0,0 +1,24 @@
+//#include "raylib.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+// C != OOP :(
+
+void* robot1(void* arg)
+{
+
+  return NULL;
+}
+
+int main(void)
+{
+  
+  pthread_t pathThreadRobot1;
+  pthread_create(&thread, NULL, robot1, NULL);
+
+  for (int i = 0; i < robotContainer.Length)
+
+
+  return 0;
+}
